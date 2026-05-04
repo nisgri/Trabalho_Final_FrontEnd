2cd4cd64dd22f305c0a2320a0243310031c1310a
rebekaoc <rebekacardoso022@gmail.com>
2026-05-04 20:44:37 -0300

    mudanças CEP, preenchimento automático

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,19 +3,23 @@ document
   .addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let endereco = document.getElementById("endereco").value;
-    let cep = document.getElementById("cep").value;
-    let pagamento = document.getElementById("pagamento").value;
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let cep = document.getElementById("cep").value.trim();
+    let pagamento = document.getElementById("pagamento").value.trim();
+    let estado = document.getElementById("estado").value.trim();
+    let bairro = document.getElementById("bairro").value.trim();
+    let logradouro = document.getElementById("logradouro").value.trim();
     let mensagem = document.getElementById("mensagem");
 
     if (
-      nome === "" ||
-      email === "" ||
-      endereco === "" ||
-      cep === "" ||
-      pagamento === ""
+      nome == "" ||
+      email == "" ||
+      cep == "" ||
+      pagamento == "" ||
+      estado == "" ||
+      bairro == "" ||
+      logradouro == ""
     ) {
       mensagem.style.color = "red";
       mensagem.textContent = "Preencha todos os campos!";
@@ -28,21 +32,21 @@ document
       return;
     }
 
-    if (cep.length < 8) {
-      mensagem.style.color = "red";
-      mensagem.textContent = "CEP inválido!";
-      return;
-    }
-
-    let response = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
-    let body = await response.json();
-
-    if (body.erro == "true") {
-      mensagem.style.color = "red";
-      mensagem.textContent = "Esse CEP não existe";
-      return;
-    }
-
     mensagem.style.color = "green";
     mensagem.textContent = "Compra realizada com sucesso!";
+  });
+
+document
+  .getElementById("cep")
+  .addEventListener("input", async function (event) {
+    let cep = document.getElementById("cep").value;
+    if (cep.length == 8) {
+      let buscaCep = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+      let dados = await buscaCep.json();
+
+      document.getElementById("estado").value = dados.estado;
+      document.getElementById("bairro").value = dados.bairro;
+      document.getElementById("logradouro").value = dados.logradouro;
+      document.getElementById("complemento").value = dados.complemento;
+    }
   });
